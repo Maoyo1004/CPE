4ccf3c3ca7b946ac0932dfa5e63982296f8a9e6e
Maoyo1004 <d534185783@gmail.com>
2025-10-07 15:41:41 +0800

    Create c045-00490-Rotating-Sentences.cpp
    Not yet

---
+++ c045-00490-Rotating-Sentences.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+#include<string>
+using namespace std ;
+
+void column() {
+	string a[105] ;
+	int b = 0 ;
+	int c = 0 ;
+	while ( getline( cin, a[ b ] ) {
+		c = max( c, (int)a[ b ].size() ) ;
+		++b ;
+	}
+	for ( int i = 0 ; i < c ; ++i ) {
+		for ( int j = b - 1 ; j >= 0 ; --j ) {
+			if ( i > a[ j ].size() ) {
+				cout << " " ;
+			} else {
+				cout << a[ j ][ i ] ;
+			}
+			cout "\n" ;
+		}
+	}
+} 
+
+int main () {
+	column() ;
+}
+	
