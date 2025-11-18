4a496487acb93f8930a37691ef03dbc33e7ee3e1
Maoyo1004 <d534185783@gmail.com>
2025-11-18 19:34:31 +0800

    Refactor Fibonacci logic and add output function
    Refactored the Fibonacci number generation and output logic. Added a new function to handle the output format for Fibonacci representation.

--- a134-00948-Fibonaccimal-Base.cpp
+++ a134-00948-Fibonaccimal-Base.cpp
@@ -1,47 +1,50 @@
 #include<iostream>
 #include<vector>
-#include<algorithm>
 using namespace std ;
 
-vector<int> fibnum ;
-void fib() {
-    int f1 = 0, f2 = 1, f3 = 1 ;
-    while ( f3 <= 100000000 ) {
-        f3 = f1 + f2 ;
-        fibnum.push_back( f3 ) ;
-        f1 = f2 ;
-        f2 = f3 ;
+void out( int a ) {
+    vector<int> fin( 2 ) ;
+    fin[ 0 ] = 1 ;
+    fin[ 1 ] = 1 ;
+    int len = 1 ;
+    while ( fin[ len ] < a ) {
+        int temp = fin[ len ] + fin[ len - 1 ] ;
+        fin.push_back( temp ) ;
+        ++len ;
     }
-    reverse( fibnum.begin(), fibnum.end() ) ;
+    vector<int> rec ;
+    while ( a != 0 ) {
+        if ( a >= fin[ len ] ) {
+            a -= fin[ len ] ;
+            rec.push_back( len ) ;
+        }
+        --len ;
+    }
+    int rec_n = 0 ;
+    bool f = false ;
+    for ( int i = fin.size() - 1 ; i > 0 ; --i ) {
+        if ( rec_n <= rec.size() - 1 ) {
+            if ( i == rec[ rec_n ] ) {
+                cout << 1 ;
+                ++rec_n ;
+                f = true ;
+            } else if ( f ) {
+                cout << 0 ;
+            }
+        } else {
+            cout << 0 ;
+        }
+    }
+    cout << " (fib)\n" ;
 }
 
 int main() {
-    fib() ;
-    int times ;
-    int num ;
-    int temp ;
-    cin >> times ;
-    int start = -1; 
-    for ( int i = 0 ; i < times ; ++i ) {
-        cin >> num ; 
-        int outnum = num ;
-        int coutnum = num ;
-        int start2 = -1 ;
-        vector<int> zero( fibnum.size(), 0 ) ;
-        for ( size_t j = 0 ; j < fibnum.size() ; ++j ) {
-            if ( coutnum >= fibnum[ j ] ) {
-                if ( start2 == -1 ) {
-                    start2 = j ;
-                }
-                coutnum -= fibnum[ j ] ;
-                zero[ j ] = 1 ;
-            }
-        }
-        cout << outnum << " = " ;
-        for ( size_t j = start2 ; j < fibnum.size() ; ++j ) {
-            cout << zero[ j ] ;
-        }
-        cout << " (fib)\n" ;
+    int t ;
+    cin >> t ;
+    for ( int i = 0 ; i < t ; ++i ) {
+        int a ;
+        cin >> a ;
+        cout << a << " = " ;
+        out( a ) ;
     }
-    return 0 ;
 }
